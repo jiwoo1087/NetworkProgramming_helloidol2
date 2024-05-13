baf543e0d4c6982fd74cc7adc8f59427f59d7809
Kim Jiwoo <88700207+jiwoo1087@users.noreply.github.com>
2024-05-13 12:16:43 +0900

    views CharacterLisetViews

--- boynextdoor/views.py
+++ boynextdoor/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from boynextdoor.models import Character
+
 
 # Create your views here.
+
+class CharacterListView(ListView):
+    modle = Character
+    # character_list = Character.objects.all() #DB에 Character 데이터 다 가져오자
+    # return render(request, 'boynextdoor.list.html', context = {'character_list : character_list})
+    # 모델_list.html에 모델_list라는 키로 DB에서 가져온 데이터 넣어서 render 하자
